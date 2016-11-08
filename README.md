b8395969ea66ea4a22666428e82a1b17a44add4a
oznu <dev@oz.nu>
2016-11-08 22:52:38 +1100

    Updated blacklist

--- README.md
+++ README.md
@@ -4,16 +4,25 @@ This project generates a BIND zone file to be used in DNS based AD Blockers such
 
 The blacklist is generated using data from the [StevenBlack/hosts](https://github.com/StevenBlack/hosts) project which is extending and consolidating hosts files from a variety of sources like adaway.org, mvps.org, malwaredomains.com, someonewhocares.org, yoyo.org, and potentially others.
 
+Since DNS based AD Blockers can support wildcard entries, this tool filters out any subdomains of known adware or malware domains, reducing the number of BIND entries required from over 29,000 to just under 19,000.
+
 ## Building the Blacklist
 
 The blacklist can be generated using [Node.js 6.9.1](https://nodejs.org) or later.
 
+Install:
+
 ```
 git clone https://github.com/oznu/bind-zone-blacklist.git
-cd bind-zone-blacklist/build
+cd bind-zone-blacklist
 
 npm install
-node index.js
+```
+
+Then build:
+
+```
+node build/index.js
 ```
 
 The updated zones.blacklist file will be saved in the root of the project.
--- build/index.js
+++ build/index.js
@@ -2,68 +2,75 @@
 
 const fs = require('fs');
 const rp = require('request-promise');
+const path = require('path');
 
-const whitelist = require('./whitelist');
-const master = [];
+const main = () => {
+  const whitelist = require('../custom.whitelist');
+  const master = require('../custom.blacklist');
+  const dest = path.resolve(__dirname, '../zones.blacklist');
 
-rp.get('https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts')
-  .then((hosts) => {
-    return hosts.split('\n');
-  })
-  .map((host) => {
-    // Trim whitespace
-    return host.trim();
-  })
-  .filter((host) => {
-    // Remove blank lines and comments
-    if (host === '') {
-      return false;
-    } else if (host.charAt(0) === '#') {
-      return false;
-    }
-    return true;
-  })
-  .map((host) => {
-    // Extract just the hostname
-    return host.split(' ')[1];
-  })
-  .filter((host) => {
-    // Filter out whitelisted domains
-    if (whitelist.includes(host)) {
-      return false;
-    }
-    return true;
-  })
-  .then((hosts) => {
-    // Sort hosts by length
-    return hosts.sort((a, b) => {
-      return a.length - b.length;
-    });
-  })
-  .map((host) => {
-    // Remove subdomains - with dns we can block using a wildcard.
-    let find = master.find(x => host.slice(-Math.abs(x.length + 1)) === '.' + x);
-    if (!find) {
-      return master.push(host);
-    }
-    console.log(host + ' is a subdomain of ' + find);
-    return;
-  }, {concurrency: 1})
-  .then(() => {
-    return master;
-  })
-  .map((host) => {
-    // Format for bind zone file
-    return `zone "${host}" { type master; notify no; file "/etc/bind/null.zone.file"; };`;
-  })
-  .then((hosts) => {
-    return hosts.join('\n');
-  })
-  .then((bind) => {
-    fs.writeFile('zones.blacklist', bind, (err) => {
-      if (err) {
-        throw err;
+  return rp.get('https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts')
+    .then((hosts) => {
+      return hosts.split('\n');
+    })
+    .map((host) => {
+      // Trim whitespace
+      return host.trim();
+    })
+    .filter((host) => {
+      // Remove blank lines and comments
+      if (host === '') {
+        return false;
+      } else if (host.charAt(0) === '#') {
+        return false;
       }
-      console.log('It\'s saved!');
+      return true;
+    })
+    .map((host) => {
+      // Extract just the hostname
+      return host.split(' ')[1];
+    })
+    .filter((host) => {
+      // Filter out whitelisted domains
+      if (whitelist.includes(host)) {
+        return false;
+      }
+      return true;
+    })
+    .then((hosts) => {
+      // Sort hosts by length
+      return hosts.sort((a, b) => {
+        return a.length - b.length;
+      });
+    })
+    .map((host) => {
+      // Remove subdomains - with dns we can block using a wildcard.
+      let find = master.find(x => host.slice(-Math.abs(x.length + 1)) === '.' + x);
+      if (!find) {
+        console.log(`Adding zone ${host}`);
+        return master.push(host);
+      }
+      return;
+    }, {concurrency: 1})
+    .then(() => {
+      return master;
+    })
+    .map((host) => {
+      // Format for bind zone file
+      return `zone "${host}" { type master; notify no; file "null.zone.file"; };`;
+    })
+    .then((hosts) => {
+      return hosts.join('\n');
+    })
+    .then((bind) => {
+      fs.writeFile(dest, bind, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(`${master.length} zones saved to ${dest}`);
+      });
     });
-  });
+
+};
+
+main();
