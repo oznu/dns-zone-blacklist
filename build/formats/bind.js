47f74a11afe91f2d34ca2e4ad1bbd81bba162329
oznu <dev@oz.nu>
2016-11-14 11:07:14 +1100

    Added sha256 checksums

--- build/formats/bind.js
+++ build/formats/bind.js
@@ -3,9 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Bluebird = require('bluebird');
+const crypto = require('crypto');
 
 module.exports = (master) => {
   const dest = path.resolve(__dirname, '../../bind/zones.blacklist');
+  const checksum = dest + '.checksum';
 
   return Bluebird.map(master, (host) => {
     return `zone "${host}" { type master; notify no; file "null.zone.file"; };`;
@@ -14,11 +16,20 @@ module.exports = (master) => {
     return hosts.join('\n');
   })
   .then((bind) => {
+
+    let sha256 = crypto.createHash('sha256').update(bind).digest('hex');
+    fs.writeFile(checksum, sha256, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`bind checksum is ${sha256}`);
+    });
+
     fs.writeFile(dest, bind, (err) => {
       if (err) {
         throw err;
       }
-      console.log(`${master.length} zones saved to ${dest}`);
+      console.log(`${master.length} bind zones saved to ${dest}`);
     });
   });
 };
--- build/formats/dnsmasq.js
+++ build/formats/dnsmasq.js
@@ -3,9 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Bluebird = require('bluebird');
+const crypto = require('crypto');
 
 module.exports = (master) => {
   const dest = path.resolve(__dirname, '../../dnsmasq/dnsmasq.blacklist');
+  const checksum = dest + '.checksum';
 
   return Bluebird.map(master, (host) => {
     return `address=/${host}/0.0.0.0`;
@@ -14,11 +16,20 @@ module.exports = (master) => {
     return hosts.join('\n');
   })
   .then((bind) => {
+
+    let sha256 = crypto.createHash('sha256').update(bind).digest('hex');
+    fs.writeFile(checksum, sha256, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`dnsmasq checksum is ${sha256}`);
+    });
+
     fs.writeFile(dest, bind, (err) => {
       if (err) {
         throw err;
       }
-      console.log(`${master.length} zones saved to ${dest}`);
+      console.log(`${master.length} dnsmasq zones saved to ${dest}`);
     });
   });
 };
