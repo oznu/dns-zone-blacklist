2ad296dc5407fdda8e111a31bcf4e273279c63c5
oznu <dev@oz.nu>
2017-06-29 15:54:41 +1000

    Updated travis-ci

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -4,16 +4,24 @@ set -x
 # Setup Git
 git config --global user.email "dev@oz.nu"
 git config --global user.name "oznu"
-git -C $TRAVIS_BUILD_DIR remote set-url origin git@github.com:oznu/dns-zone-blacklist.git
+
+# Clone a fresh instance using the current branch
+git clone -b $BRANCH git@github.com:oznu/dns-zone-blacklist.git $TRAVIS_BUILD_DIR/travis-build-dir
+cd $TRAVIS_BUILD_DIR/travis-build-dir
+
+# Install dependencies
+yarn install
+
+# Build Blacklist
+node build/index.js
 
 # Check to see if we made any changes
-git -C $TRAVIS_BUILD_DIR diff --quiet && git -C $TRAVIS_BUILD_DIR diff --cached --quiet
-
+git diff --quiet && git diff --cached --quiet
 if [ $? -ne 0 ]; then
   # Commit and push changes
   echo "DNS Blacklist requires update. Committing and Pushing."
-  git -C $TRAVIS_BUILD_DIR commit -a -m 'Automated Update'
-  git -C $TRAVIS_BUILD_DIR push origin $BRANCH
+  git commit -a -m 'Automated Update'
+  git push
 else
   # No changes made
   echo "DNS Blacklist is already up-to-date!"
