c6d403dcca53ed5c0a27588875fa6d0ec597f0c6
oznu <dev@oz.nu>
2017-06-29 15:13:21 +1000

    Updated travis-ci

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-git -C $WORK_DIR diff --quiet && git -C $WORK_DIR diff --cached --quiet
+git -C $TRAVIS_BUILD_DIR diff --quiet && git -C $TRAVIS_BUILD_DIR diff --cached --quiet
 
 if [ $? -ne 0 ]; then
 	echo "DNS Blacklist requires update. Committing and Pushing."
-	# git -C $WORK_DIR commit -a -m 'Automated Update'
-  #      git -C $WORK_DIR push
+	git -C $TRAVIS_BUILD_DIR commit -a -m 'Automated Update'
+  git -C $TRAVIS_BUILD_DIR push
 else
 	echo "DNS Blacklist is already up-to-date!"
 fi
