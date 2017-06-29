58200d050c9d0ff8543ff568fbd667fc1b7a7e5c
oznu <oznu@users.noreply.github.com>
2017-06-29 16:08:45 +1000

    Use Travis CI to do automatic updates

---
+++ .travis-ci/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Setup Git
+git config --global user.email "dev@oz.nu"
+git config --global user.name "oznu"
+
+# Clone a fresh instance using the current branch
+git clone -b $BRANCH git@github.com:oznu/dns-zone-blacklist.git $TRAVIS_BUILD_DIR/travis-build-dir
+cd $TRAVIS_BUILD_DIR/travis-build-dir
+
+# Install dependencies
+yarn install
+
+# Build Blacklist
+echo "Optimising blacklist, this might take a few minutes..."
+node build/index.js --silent
+
+# Check to see if we made any changes
+git diff --quiet && git diff --cached --quiet
+if [ $? -ne 0 ]; then
+  # Commit and push changes
+  echo "DNS Blacklist requires update. Committing and Pushing."
+  git commit -a -m 'Automated Update'
+  git push
+else
+  # No changes made
+  echo "DNS Blacklist is already up-to-date!"
+fi
