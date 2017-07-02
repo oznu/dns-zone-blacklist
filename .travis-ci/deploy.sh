eea3ba061a45593600eefdd80386a85cd67c3c0e
oznu <dev@oz.nu>
2017-07-02 17:42:39 +1000

    Updated deploy script

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -5,7 +5,7 @@ git config --global user.email "dev@oz.nu"
 git config --global user.name "oznu"
 
 # Clone a fresh instance using the current branch
-git clone -b $BRANCH git@github.com:oznu/dns-zone-blacklist.git $TRAVIS_BUILD_DIR/travis-build-dir
+git clone -b $BRANCH --depth=2 git@github.com:oznu/dns-zone-blacklist.git $TRAVIS_BUILD_DIR/travis-build-dir
 cd $TRAVIS_BUILD_DIR/travis-build-dir
 
 # Install dependencies
