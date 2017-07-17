c602f3b7bd7565999cf6a07821fc170ee892c12d
oznu <dev@oz.nu>
2017-07-17 16:30:13 +1000

    Adjusting CI

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Setup SSH Keys
+openssl aes-256-cbc -K $encrypted_b281b2f14922_key -iv $encrypted_b281b2f14922_iv -in .travis-ci/deploy.key.enc -out .travis-ci/deploy.key -d
+chmod 400 .travis-ci/deploy.key
+eval `ssh-agent -s`
+ssh-add .travis-ci/deploy.key
+
 # Setup Git
 git config --global push.default simple
 git config user.email "dev@oz.nu"
@@ -12,3 +18,6 @@ git remote add github git@github.com:oznu/dns-zone-blacklist.git
 git add .
 git commit -a -m 'Automated Update' || echo "Blacklist Already Up-to-date"
 git push github
+
+# Clean up ssh
+ssh-agent -k
