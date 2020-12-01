72b9235a18a3ead97a92ade98c230d842634a765
euh2 <tb@tobeon.net>
2020-12-01 18:30:31 +0100

    Allow to disable formats, and allow to have non-default custom formats, ie. custom.formats.json M  .gitignore M  build.js A  custom.formats.json.example

--- build.js
+++ build.js
@@ -10,40 +10,57 @@ class Blacklist {
   constructor () {
     this.whitelist = require('./custom.whitelist')
     this.blacklist = require('./custom.blacklist')
-
-    this.formats = [
-      {
-        type: 'bind',
-        filename: 'zones.blacklist',
-        template: 'zone "<%= host %>" { type master; notify no; file "null.zone.file"; };'
-      },
-      {
-        type: 'bind',
-        filename: 'bind-nxdomain.blacklist',
-        template: '<%= host %> CNAME .\n*.<%= host %> CNAME .',
-        header: `$TTL 60\n@ IN SOA localhost. dns-zone-blacklist. (2 3H 1H 1W 1H)\ndns-zone-blacklist. IN NS localhost.`
-      },
-      {
-        type: 'dnsmasq',
-        filename: 'dnsmasq.blacklist',
-        template: 'address=/<%= host %>/0.0.0.0'
-      },
-      {
-        type: 'dnsmasq',
-        filename: 'dnsmasq-server.blacklist',
-        template: 'server=/<%= host %>/'
-      },
-      {
-        type: 'unbound',
-        filename: 'unbound.blacklist',
-        template: 'local-zone: "<%= host %>" redirect\nlocal-data: "<%= host %> A 0.0.0.0"'
-      },
-      {
-        type: 'unbound',
-        filename: 'unbound-nxdomain.blacklist',
-        template: 'local-zone: "<%= host %>" always_nxdomain'
+    // check for custom.formats file
+    let custom_formats = './custom.formats.json1'
+    fs.access(custom_formats, fs.F_OK, (nofile) => {
+      if (nofile) {
+        // file does not exist
+        console.log(`Using default formats, no ${custom_formats} file found.`)
+        this.formats = [
+          {
+            type: 'bind',
+            filename: 'zones.blacklist',
+            enabled: true,
+            template: 'zone "<%= host %>" { type master; notify no; file "null.zone.file"; };'
+          },
+          {
+            type: 'bind',
+            filename: 'bind-nxdomain.blacklist',
+            enabled: true,
+            template: '<%= host %> CNAME .\n*.<%= host %> CNAME .',
+            header: `$TTL 60\n@ IN SOA localhost. dns-zone-blacklist. (2 3H 1H 1W 1H)\ndns-zone-blacklist. IN NS localhost.`
+          },
+          {
+            type: 'dnsmasq',
+            filename: 'dnsmasq.blacklist',
+            enabled: true,
+            template: 'address=/<%= host %>/0.0.0.0'
+          },
+          {
+            type: 'dnsmasq',
+            filename: 'dnsmasq-server.blacklist',
+            enabled: true,
+            template: 'server=/<%= host %>/'
+          },
+          {
+            type: 'unbound',
+            filename: 'unbound.blacklist',
+            enabled: true,
+            template: 'local-zone: "<%= host %>" redirect\nlocal-data: "<%= host %> A 0.0.0.0"'
+          },
+          {
+            type: 'unbound',
+            filename: 'unbound-nxdomain.blacklist',
+            enabled: true,
+            template: 'local-zone: "<%= host %>" always_nxdomain'
+          }
+        ]
+        return
       }
-    ]
+      //file exists
+      console.log(`Using custom formats found in ${path}.`)
+      this.formats = require('./custom.formats')
+    })
   }
 
   async build () {
@@ -66,21 +83,30 @@ class Blacklist {
 
     // Build a zone blacklist for each format type
     this.formats.forEach((format) => {
-      let zoneFile = this.blacklist.map(x => ejs.render(format.template, {host: x})).join('\n')
+      if (format.enabled) {
+        let zoneFile = this.blacklist.map(x => ejs.render(format.template, {host: x})).join('\n')
 
-      if (format.header) {
-        zoneFile = format.header + '\n\n' + zoneFile
+        if (format.header) {
+          zoneFile = format.header + '\n\n' + zoneFile
+        }
+
+        let sha256 = crypto.createHash('sha256').update(zoneFile).digest('hex')
+        let dest = path.resolve(__dirname, `${format.type}/${format.filename}`)
+
+        fs.writeFileSync(`${dest}.checksum`, sha256)
+        console.log(`${format.filename} checksum is ${sha256}`)
+
+        fs.writeFileSync(`${dest}`, zoneFile)
+        console.log(`${this.blacklist.length} ${format.filename} zones saved to ${dest}`)
       }
-
-      let sha256 = crypto.createHash('sha256').update(zoneFile).digest('hex')
-      let dest = path.resolve(__dirname, `${format.type}/${format.filename}`)
-
-      fs.writeFileSync(`${dest}.checksum`, sha256)
-      console.log(`${format.filename} checksum is ${sha256}`)
-
-      fs.writeFileSync(`${dest}`, zoneFile)
-      console.log(`${this.blacklist.length} ${format.filename} zones saved to ${dest}`)
     })
+
+    // write array to json file
+    let custom_formats_file = JSON.stringify(this.formats, null, 2);
+    fs.writeFile('custom.formats.json', custom_formats_file, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    }); 
 
     // Update the README.md file
     let readmeTemplate = fs.readFileSync(path.resolve(__dirname, 'README.template.md'), 'utf8')
